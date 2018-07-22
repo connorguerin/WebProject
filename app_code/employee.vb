c09933794d4dc36eb1b7152ea21c83d4d2cfdbca
connorguerin <connorguerin239@gmail.com>
2018-07-22 19:56:00 -0400

    Update employee.vb

--- app_code/employee.vb
+++ app_code/employee.vb
@@ -1,15 +1,1 @@
-Public Class Employee
-    Private ReadOnly staffID As Integer
-    Private ReadOnly positionID As Integer
-    Private ReadOnly Name As Integer
-    Private ReadOnly DateHired As DateTime
-    Private ReadOnly SkillLevel As Integer
 
-    Public Sub New(aStaffID, aPositionID, aName, aDateHired)
-        staffID = aStaffID
-        positionID = aPositionID
-        Name = aName
-        DateHired = aDateHired
-        SkillLevel = DateDiff(DateInterval.Year, Today(), aDateHired())
-    End Sub
-End Class
