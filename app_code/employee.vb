faa2925ddf5a67bb8957fbd115a69be0411419f5
connorguerin <connorguerin239@gmail.com>
2018-07-22 20:33:46 -0400

    Update employee.vb

--- app_code/employee.vb
+++ app_code/employee.vb
@@ -3,8 +3,6 @@ Public Class Employee
     Private ReadOnly positionID As Integer
     Private ReadOnly Name As String
     Private ReadOnly DateHired As DateTime
-    Private ReadOnly SkillLevel As Integer
-    Private ReadOnly HourlyPayRate As Double
 
     Public Sub New(aStaffID As Integer, aPositionID As Integer, aName As String, aDateHired As DateTime)
         staffID = aStaffID
@@ -12,6 +10,19 @@ Public Class Employee
         Name = aName
         DateHired = aDateHired
         SkillLevel = DateDiff(DateInterval.Year, Today(), DateHired)
+
+    End Sub
+
+    Public Function CanWorkJob(aJobID As Integer) As Integer
+        If (aJobID * 2 = positionID Or aJobID * 2 - 1 = positionID) Then
+            Return 1
+        Else
+            Return 0
+        End If
+
+    End Function
+
+    Public Function HourlyRate() As Double
         'this is ugly as fuck hard coding all this stuff
         'TODO determine payrates
         If (staffID = 1) Then
@@ -40,14 +51,8 @@ Public Class Employee
 
 
         End If
-    End Sub
-
-    Public Function CanWorkJob(aJobID As Integer) As Integer
-        If (aJobID * 2 = positionID Or aJobID * 2 - 1 = positionID) Then
-            Return 1
-        Else
-            Return 0
-        End If
+    End Function
+    Public Function SkillRating() As Integer
 
     End Function
 
