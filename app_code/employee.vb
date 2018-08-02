8cb62c61b6b5c5de4c51b79f0b57a40e13560f53
connorguerin <connorguerin239@gmail.com>
2018-08-02 16:26:18 -0400

    Update employee.vb

--- app_code/employee.vb
+++ app_code/employee.vb
@@ -1,18 +1,30 @@
+Imports Microsoft.VisualBasic
+
+
 Public Class Employee
     Private ReadOnly staffID As Integer
     Private ReadOnly positionID As Integer
     Private ReadOnly Name As String
     Private ReadOnly DateHired As DateTime
+    Private ReadOnly BaseRate As Integer
+    Private AvailableHours As Integer
 
-    Public Sub New(aStaffID As Integer, aPositionID As Integer, aName As String, aDateHired As DateTime)
+    Public Sub New(aStaffID As Integer, aPositionID As Integer, aName As String, aDateHired As DateTime, Base As Integer)
         staffID = aStaffID
         positionID = aPositionID
         Name = aName
         DateHired = aDateHired
+        BaseRate = Base
+        If positionID Mod 2 = 1 Then
+            AvailableHours = 50
+        Else
+            AvailableHours = 30
+        End If
+
     End Sub
 
     Public Function CanWorkJob(aJobID As Integer) As Integer
-        If (aJobID * 2 = positionID Or aJobID * 2 - 1 = positionID) Then
+        If ((aJobID + 1) * 2 = positionID Or (aJobID + 1) * 2 - 1 = positionID) Then
             Return 1
         Else
             Return 0
@@ -21,54 +33,11 @@ Public Class Employee
     End Function
 
     Public Function HourlyRate() As Double
-        'this is ugly as fuck hard coding all this stuff
-        'TODO determine payrates
-        Dim baseRate As Integer
-        If (staffID = 1) Then
-            baseRate = 20
-        End If
-
-        If (staffID = 2) Then
-            baseRate = 10
-        End If
-
-        If (staffID = 3) Then
-            baseRate = 22
-        End If
-        If (staffID = 4) Then
-            baseRate = 12
-        End If
-
-        If (staffID = 5) Then
-            baseRate = 24
-        End If
-        If (staffID = 6) Then
-            baseRate = 14
-        End If
-        If (staffID = 7) Then
-            baseRate = 22
-        End If
-
-        If (staffID = 8) Then
-            baseRate = 12
-        End If
-        If (staffID = 9) Then
-            baseRate = 23
-        End If
-        If (staffID = 10) Then
-            baseRate = 13
-        End If
-        If (staffID = 11) Then
-            baseRate = 24
-        End If
-        If (staffID = 12) Then
-            baseRate = 14
-        End If
-
-        Return baseRate * (1 + (0.05 * SkillRating()))
+        Return BaseRate * (1 + (0.05 * SkillRating()))
     End Function
+
     Public Function SkillRating() As Integer
-        Return math.abs(DateDiff(DateInterval.Year, Today(), DateHired))
+        Return Math.Abs(DateDiff(DateInterval.Year, Today(), DateHired))
     End Function
 
     Public Function MaxHours() As Integer
@@ -86,4 +55,26 @@ Public Class Employee
             Return 40
         End If
     End Function
+
+    Public ReadOnly Property GetHours() As Integer
+        Get
+            Return AvailableHours
+        End Get
+    End Property
+
+    Public Sub DecreaseHours()
+        AvailableHours -= 4
+    End Sub
+
+    Public ReadOnly Property GetID As Integer
+        Get
+            Return staffID
+        End Get
+    End Property
+
+    Public ReadOnly Property GetName As String
+        Get
+            Return Name
+        End Get
+    End Property
 End Class
